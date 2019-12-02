3a25e266feb2d9003fe22f41e0b61513880f051f
Moritz Weiss <moritz.weiss95@hotmail.de>
2019-12-02 17:26:44 +1100

    added some tests more tests

--- src/Vehicle.test.ts
+++ src/Vehicle.test.ts
@@ -8,6 +8,39 @@ var car = new Vehicle({
 })
 
 
-test("teste das hier", () => {
+test("Check attributes after initializing", () => {
     expect(car.horsepower).toBe(270)
+    expect(car.color).toBe("#CCCCFF")
+    expect(car.seats).toBe(5)
+    expect(car.wheels).toBe(4)
+})
+
+test("Check starting the engine with failure", () => {
+    expect(car.startEngine()).toEqual(false)    
+})
+
+test("Check refueling the tank to half", () => {
+    car.refuel(50)
+    expect(car.getFuelLevel()).toEqual(50)
+})
+
+test("Check refueling with a numeric string", () => {
+    // @ts-ignore
+    car.refuel("20")
+    expect(car.getFuelLevel()).toBe(70)
+})
+
+test("Check refueling with a string", () => {
+    // @ts-ignore
+    car.refuel("ab")
+    expect(car.getFuelLevel()).toBe(70)
+})
+
+test("Check starting the engine with success", () => {
+    expect(car.startEngine()).toEqual(true)    
+})
+
+test("Crashing the car", () => {
+    car.crash()
+    expect(car.startEngine()).toEqual(false)
 })
--- src/Vehicle.ts
+++ src/Vehicle.ts
@@ -5,34 +5,47 @@ interface vehicleSettings {
     color: string
 }
 
-export default class Vehicle {
+class Vehicle {
 
     public seats: number
     public wheels: number
     public horsepower: number
     public color: string
-    private fuelLevel: number
+    private crashed: boolean = false
+    private fuelLevel: number = 0
 
     constructor(settings: vehicleSettings) {
-        for (var setting in settings) {
+        for (let setting in settings) {
             this[setting] = settings[setting]
         }
     }
 
-    public refuel(percentage: number) {
-        var totalLevel = this.fuelLevel + percentage
-        this.fuelLevel = totalLevel > 100 ? 100 : totalLevel;
+    public refuel(percentage: number): void {
+        let totalLevel = this.fuelLevel + (+percentage || 0)
+        this.fuelLevel = totalLevel > 100 ? 100 : totalLevel
         console.log("Tank is " + this.fuelLevel + "% filled up.")
         if (totalLevel > 100) console.log("Fuel spilled on the ground!")
     }
 
+    public getFuelLevel(): number {
+        return this.fuelLevel
+    }
+
+    public crash(): void {
+        if(!this.crashed) {
+            this.wheels = this.wheels - 1
+            this.crashed = true
+        } 
+    }
+
+    public repair(): void {
+        this.wheels = 4
+        this.crashed = false
+    }
+
+    public startEngine(): boolean {
+        return !(this.fuelLevel == 0 || this.crashed) 
+    }
 }
 
-// var myCarSettings: vehicleSettings = {
-//     seats: 5,
-//     wheels: 4,
-//     horsepower: 270,
-//     color: '#CCCCFF'
-// }
-// var myCar = new Vehicle(myCarSettings)
-
+export default Vehicle
